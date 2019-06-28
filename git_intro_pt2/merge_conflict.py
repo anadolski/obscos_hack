bf1fea81ff4dde0f6c844ebd4ad048d638438eba
sreevanij <jsv1206@gmail.com>
2019-06-28 14:50:36 -0500

    added brakets in branch

--- git_intro_pt2/merge_conflict.py
+++ git_intro_pt2/merge_conflict.py
@@ -2,7 +2,7 @@ import numpy as np
 
 # This is the function that we will edit
 for i in np.linspace(141, 1e6, num=5):
-    print('{}'.format(i)
+    print('{}'.format(i))))
 
 for j in range(0,5):
     print(j)
