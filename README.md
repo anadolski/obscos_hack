0812e9c2ab836e52b07a369f19ac68fbec2e395b
Andrew Nadolski <andrew.nadolski@gmail.com>
2019-06-28 13:18:50 -0500

    Initial commit to h4xx0r5 sandbox
    Added a README and our merge conflcit test file.

---
+++ README.md
@@ -0,0 +1,3 @@
+# ObsCos 1337 h4XX0r5
+This is a sandbox for the obscos h4xx0r5 (and any other interested parties)
+
---
+++ git_intro_pt2/merge_conflict.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+# This is the function that we will edit
+for i in np.linspace(0, 1000):
+    print('{}'.format(i)
